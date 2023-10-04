2a21dca0f700e630fb2af84a94edd185ff96f263
victordwiagustin <victor.agustin@formulatrix.com>
2023-10-04 08:41:11 +0700

    Add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import moneyTreeLogo from './assets/money-tree.svg';
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <img src={moneyTreeLogo} className="logo" alt="Money" width={100} />
       <div>Balance-calc-expr dashboard</div>
     </>
@@ -11,5 +14,3 @@ function App() {
 }
 
 export default App;
-
-// TODO: todos on Readme.md file
